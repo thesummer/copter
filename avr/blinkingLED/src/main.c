96f72d288a1e0252bd6fc206a57c9dc1eb8c1e75
Jan Sommer <jsom@hrz.tu-chemnitz.de>
2012-07-04 23:21:49 +0200

    Some changes to the blinking LED example
    Instead of using interrupt on compare match the mode to toggle the OCA1 pin (PB3) was used
    by setting the COM1A0 bit of the TCCR1A register. LED was then connected to the PB3-pin.
    Results are the same, but the program turned out to be 128-92 = 36 bytes smaller
    
    Also added the data sheet of the Attiny2313

--- avr/blinkingLED/src/main.c
+++ avr/blinkingLED/src/main.c
@@ -15,23 +15,30 @@
 int main(void)
 {
 
+  //Initialize OC1A pin (Pin PB3) for LED
+  
+  DDRB  |= 1<<DDB3;		//Set PortB Pin3 as an output (OC1A)
+  PORTB |= 1<<PINB3;		//Set PortB Pin3 high to turn on LED
+      
   //Setup the clock
   cli();			//Disable global interrupts
   TCCR1B |= 1<<CS11 | 1<<CS10;	//Divide by 64
   OCR1A = 15624;		//Count 15624 cycles for 1 second interrupt
   TCCR1B |= 1<<WGM12;		//Put Timer/Counter1 in CTC mode
-  TIMSK |= 1<<OCIE1A;		//enable timer compare interrupt
+  TCCR1A |= 1<<COM1A0;		//Toggle OC1A pin on compare match
+//   TIMSK |= 1<<OCIE1A;		//enable timer compare interrupt
   sei();			//Enable global interrupts
 
   //Setup the I/O for the LED
 
-  DDRD |= (1<<0);		//Set PortD Pin0 as an output
-  PORTD |= (1<<0);		//Set PortD Pin0 high to turn on LED
+//   DDRD |= (1<<0);		//Set PortD Pin0 as an output
+//   PORTD |= (1<<0);		//Set PortD Pin0 high to turn on LED
 
+  
   while(1) { }			//Loop forever, interrupts do the rest
 }
 
-ISR(TIMER1_COMPA_vect)		//Interrupt Service Routine
-{
-  PORTD ^= (1<<0);		//Use xor to toggle the LED
-}
+// ISR(TIMER1_COMPA_vect)		//Interrupt Service Routine
+// {
+//   PORTD ^= (1<<0);		//Use xor to toggle the LED
+// }
