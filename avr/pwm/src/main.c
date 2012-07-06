f6a449ca0b0b8138af26fae71ddd6b83a61b1a7b
Jan Sommer <jsom@hrz.tu-chemnitz.de>
2012-07-06 14:16:03 +0200

    All 4 PWM channels working in 8-bit mode.

--- avr/pwm/src/main.c
+++ avr/pwm/src/main.c
@@ -1,37 +1,81 @@
 /*
-* Hackaday.com AVR Tutorial firmware
-* written by: Mike Szczys (@szczys)
-* 10/24/2010
+* written by: Jan Sommer
+* 05.07.2012
 *
 * ATtiny2313
-* Blinks one LED conneced to PD0
+* Blinks one LED conneced to PD0 every second
+* Dimms a seond LED connected to PB2, dimming changes every second (together with first LED)
 *
-* http://hackaday.com/2010/10/25/avr-programming-02-the-hardware/
 */
+
+#define duty1	OCR0A
+#define duty2	OCR0B
+#define duty3	OCR1A
+#define duty4	OCR1B
+
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include <util/delay.h>
+
 
 int main(void)
 {
 
-  //Setup the clock
-  cli();			//Disable global interrupts
-  TCCR1B |= 1<<CS11 | 1<<CS10;	//Divide by 64
-  OCR1A = 15624;		//Count 15624 cycles for 1 second interrupt
-  TCCR1B |= 1<<WGM12;		//Put Timer/Counter1 in CTC mode
-  TIMSK |= 1<<OCIE1A;		//enable timer compare interrupt
+  //Setup the I/O for the LEDs
+
+  //LED1
+  DDRD  |= 1<<DDD5;		//Set PortD Pin5 (OC0B) as an output
+
+  //LED2
+  DDRB  |= 1<<DDB2;		//Set PortB Pin2 (OC0A) as an output
+  DDRB  |= 1<<DDB3;		//Set PortB Pin3 (OC1A) as an output
+  DDRB  |= 1<<DDB4;		//Set PortB Pin4 (O1BA) as an output
+      
+  //Setup the clocks
+  cli();				//Disable global interrupts
+  
+  //Setup Timer0
+//   TCCR0B |= 1<<CS01  | 1<<CS00;	//Divide by 64
+  TCCR0A |= (1<<WGM00);			//Set phase correct PWM
+  TCCR0A |= (1<<COM0A1) | (1<<COM0B1);	//Set both output pins to non-inverting PWM
+  TCCR0B |=  1<<CS01;			//Divide by 8
+  
+  //Setup Timer1
+//   TCCR1B |= 1<<CS11 | 1<<CS10;	//Divide by 64
+  TCCR1B |= 1<<CS11;			//Divide by 8
+  TCCR1A |= 1<<WGM10;			//Set phase correct PWM 8-Bit
+  TCCR1A |= 1<<COM1A1 | 1<<COM1B1;	//Activate both output pins to non-inverting pwm
+  
+  //Initialize duty cycles at the beginning
+  duty1 = 1;				
+  duty2 = 255;
+  duty3 = 1;
+  duty4 = 255;
+  
   sei();			//Enable global interrupts
 
-  //Setup the I/O for the LED
-
-  DDRD |= (1<<0);		//Set PortD Pin0 as an output
-  PORTD |= (1<<0);		//Set PortD Pin0 high to turn on LED
-
-  while(1) { }			//Loop forever, interrupts do the rest
-}
-
-ISR(TIMER1_COMPA_vect)		//Interrupt Service Routine
-{
-  PORTD ^= (1<<0);		//Use xor to toggle the LED
+  int i;
+  while(1) 
+  {
+      for(i=0; i<7; i++)
+      {
+	    duty1 *=2;
+	    duty2 /=2;
+	    duty3 *=2;
+	    duty4 /=2;
+	    _delay_ms(150);
+      }
+      _delay_ms(1000);
+      
+      for(i=0; i<7; i++)
+      {
+	    duty1 /=2;
+	    duty2 *=2;
+	    duty3 /=2;
+	    duty4 *=2;
+	    _delay_ms(150);
+      }
+      _delay_ms(1000);
+  }
 }
