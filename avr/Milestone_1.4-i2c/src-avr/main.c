e8510a76a7fd86d03803b558cf5b09bf62468ae6
Jan Sommer <jsom@hrz.tu-chemnitz.de>
2012-07-18 19:11:10 +0200

    I2C slave device and corresponding master program with PWM signal for ESC Resolution of the control is now 13 bits. Does not work stable, yet.

---
+++ avr/Milestone_1.4-i2c/src-avr/main.c
@@ -0,0 +1,165 @@
+/*##############################################################################
+
+	Name	: USI TWI Slave driver - I2C/TWI-EEPROM
+	Version	: 1.3  - Stable
+	autor	: Martin Junghans	jtronics@gmx.de
+	page	: www.jtronics.de
+	License	: GNU General Public License 
+
+	Created from Atmel source files for Application Note AVR312: 
+  Using the USI Module as an I2C slave like a I2C-EEPROM.
+  
+//############################################################################*/
+
+
+#include 	<stdlib.h>
+#include 	<avr/io.h>
+#include 	<avr/interrupt.h>
+#include 	<avr/pgmspace.h>
+#include    <stdint.h>
+#include    <util/atomic.h>
+
+//################################################################## USI-TWI-I2C
+
+#include 	"usiTwiSlave.h"     		
+
+// Note: The LSB is the I2C r/w flag and must not be used for addressing!
+#define 	SLAVE_ADDR_ATTINY       0b00110100
+
+#ifndef 	F_CPU
+#define 	F_CPU 8000000UL
+#endif
+
+//####################################################################### Macros
+
+#define uniq(LOW,HEIGHT)	((HEIGHT << 8)|LOW)			  // Create 16 bit number from two bytes
+#define LOW_BYTE(x)        	(x & 0xff)					    // Get low byte from 16 bit number  
+#define HIGH_BYTE(x)       	((x >> 8) & 0xff)			  // Get high byte from 16 bit number
+
+#define sbi(ADDRESS,BIT) 	((ADDRESS) |= (1<<(BIT)))	// Set bit
+#define cbi(ADDRESS,BIT) 	((ADDRESS) &= ~(1<<(BIT)))// Clear bit
+#define	toggle(ADDRESS,BIT)	((ADDRESS) ^= (1<<BIT))	// Toggle bit
+
+#define	bis(ADDRESS,BIT)	(ADDRESS & (1<<BIT))		  // Is bit set?
+#define	bic(ADDRESS,BIT)	(!(ADDRESS & (1<<BIT)))		// Is bit clear?
+
+//#################################################################### Variables
+
+    #define ch0 PORTD5
+    #define ch1 PORTB2
+    #define ch2 PORTB3
+    #define ch3 PORTB4
+
+    static uint8_t onCounter;  //Stores the next channel to turn on
+    static uint8_t offCounter; //Stores the next channel to turn off
+
+    static uint16_t onValues[4] = {0, 8191, 16383, 24575};       //OCRA1 values for every ms
+    static uint16_t dutyCycles[4] = {4095, 12287, 20479, 28671}; //Stores the duty cycles for each channel
+
+
+//################################################################# Main routine
+static void PwmInit(void);
+
+int main(void)
+{	 
+    int i;
+
+    cli();  // Disable interrupts
+	
+	usiTwiSlaveInit(SLAVE_ADDR_ATTINY);	// TWI slave init
+    //Initialize rxbuffer
+    for(i=0; i<3; i++)
+    {
+        rxbuffer[i*2]   = HIGH_BYTE(dutyCycles[i]);
+        rxbuffer[i*2+1] = LOW_BYTE(dutyCycles[i]);
+    }
+
+    PwmInit();                          // PWM init
+	
+	sei();  // Re-enable interrupts
+
+    while(1)
+    {
+        for (i=0; i<3; i++)
+        {
+            ATOMIC_BLOCK(ATOMIC_FORCEON)
+            {
+                //Update duty cycles and txbuffer
+                dutyCycles[i] = uniq(rxbuffer[i*2+1], rxbuffer[i*2]);
+                txbuffer[i*2]   = rxbuffer[i*2];
+                txbuffer[i*2+1] = rxbuffer[i*2+1];
+            }
+        }
+    } //end.while
+} //end.main
+
+static void PwmInit()
+{
+      //Set output pins of all 4 channels to output
+      sbi(DDRD, DDD5);		//pin PD5 (OC0B): ch0
+      sbi(DDRB, DDB2);		//pin PB2 (OC0A): ch1
+      sbi(DDRB, DDB3);		//pin PB3 (OC1A): ch2
+      sbi(DDRB, DDB4);		//pin PB4 (OC1B): ch3
+      
+      //Setup timer1
+      sbi(TCCR1B, WGM13);	//CTC mode with ICR1 as TOP register
+      sbi(TCCR1B, WGM12);
+
+      ICR1 = (1<<15)-1;     //Set Top value to 2^15-1 == 4ms at 8MHz
+      sbi(TIMSK, ICIE1);   //Activate interrupt at TOP
+      sbi(TIMSK, OCIE1A);  //Activate interrupt for OCR1A register
+      sbi(TIMSK, OCIE1B);  //Activate interrupt for OCR1B register
+
+      //Initialize registers and variables
+      OCR1A = onValues[0];
+      OCR1B = dutyCycles[0];
+      onCounter  = 1;
+      offCounter = 0;
+
+      sbi(PORTD, ch0);      //Set ch0 high
+      sbi(TCCR1B, CS10);    //Start clock with prescaler 1
+}
+
+/*
+  ISR for the TOP value of Timer1
+  Begin of pwm-cycle:
+*/
+ISR(TIMER1_CAPT_vect)
+{
+    sbi(PORTD, ch0);    //Set ch0 high
+    OCR1A = 8191;       //Next OCR1A interrupt after 1ms
+    onCounter = 1;
+    offCounter = 0;
+}
+
+/*
+  ISR for the compare match of OCR1A of Timer1
+*/
+ISR(TIMER1_COMPA_vect)
+{
+    //Switch on correct channel and increase onCounter
+    switch (onCounter++)
+    {
+    case 0: sbi(PORTD, ch0); break;
+    case 1: sbi(PORTB, ch1); break;
+    case 2: sbi(PORTB, ch2); break;
+    case 3: sbi(PORTB, ch3); break;
+    }
+    //Set next compare interrupt (turn on next channel) in 1 ms
+    OCR1A = onValues[onCounter];
+}
+
+/*
+  ISR for the compare match of OCR1B of Timer1
+*/
+ISR(TIMER1_COMPB_vect)
+{
+    switch (offCounter++)
+    {
+    case 0: cbi(PORTB, ch3); break;
+    case 1: cbi(PORTD, ch0); break;
+    case 2: cbi(PORTB, ch1); break;
+    case 3: cbi(PORTB, ch2); break;
+    }
+    OCR1B = dutyCycles[offCounter];
+}
---
+++ avr/Milestone_1.4-i2c/src-master/main.c
@@ -0,0 +1,177 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <linux/i2c-dev.h>
+#include <stdint.h>
+#include <ncurses.h>
+
+#define I2CPORT "/dev/i2c-0"
+#define PWM_SLAVE_ADDR  0b0011010
+#define STARTREGISTER 0
+// #define FALSE 1
+// #define TRUE 06
+#define LENGTH 62
+
+#define uniq(LOW,HEIGHT)	((HEIGHT << 8)|LOW)			  // Create 16 bit number from two bytes
+#define LOW_BYTE(x)        	(x & 0xff)					    // Get low byte from 16 bit number
+#define HIGH_BYTE(x)       	((x >> 8) & 0xff)			  // Get high byte from 16 bit number
+
+uint16_t channel[4] = {0,0,0,0};
+char row[LENGTH+1];
+int pwmSlave;
+
+int I2Cinit()
+{
+      pwmSlave = open(I2CPORT, O_RDWR | O_NOCTTY | O_NDELAY);
+      if (pwmSlave == -1)
+      {
+            printf("open_port: Unable to open i2c-bus. Maybe root permissions necessary?\n");
+            return FALSE; //0 == error
+      }
+      
+      if (ioctl(pwmSlave, I2C_SLAVE, PWM_SLAVE_ADDR) < 0)
+      {
+            printf("Failed to acquire bus access and/or talk to slave.\n");
+            return FALSE; // 0 == error
+      }
+        
+      return TRUE;     
+}
+
+int setSingleChannel(int ch)
+{
+    uint8_t data[3];
+    if (channel[ch] > 8191)
+        channel[ch] = 8191;
+    if (channel[ch] < 0)
+        channel[ch] = 0;
+    
+    data[0] = STARTREGISTER + 2*ch;
+    data[1] = HIGH_BYTE(channel[ch]);
+    data[2] = LOW_BYTE(channel[ch]);
+    if (write(pwmSlave, data, 3) != 3)
+     {
+       endwin();
+       printf("Failed write duty cycle\n");
+       exit (1);
+     }
+//     else
+//        printf("Sending new duty cycle succeeded\n");
+     
+      return TRUE;
+}
+
+int setAllChannels()
+{
+    uint8_t data[9];
+    int i;
+    data[0] = STARTREGISTER;
+    for (i=0; i<4;i++)
+    {
+        if (channel[i] > 8191)
+            channel[i] = 8191;
+        if (channel[i] < 0)
+            channel[i] = 0;
+        data[2*i]   = HIGH_BYTE(channel[i]);
+        data[2*i+1] = LOW_BYTE(channel[i]);
+    }
+    
+    if (write(pwmSlave, data, 9) != 9)
+     {
+       endwin();
+       printf("Failed write duty cycles\n");
+       exit (1);
+     }
+//      else
+//        printf("Sending new duty cycles succeeded\n");
+      return TRUE;
+}
+
+void printScreen()
+{
+   erase();
+   mvprintw(2, 2, "Channel 1:");
+   mvprintw(6, 2, "Channel 2:");
+   mvprintw(10, 2, "Channel 3:");
+   mvprintw(14, 2, "Channel 4:");
+   
+   attron(COLOR_PAIR(1) | A_INVIS);
+   mvprintw(4, 5, "%s", &row[LENGTH - LENGTH*channel[0]/8191]);
+   mvprintw(8, 5, "%s", &row[LENGTH - LENGTH*channel[1]/8191]);
+   mvprintw(12, 5, "%s", &row[LENGTH - LENGTH*channel[2]/8191]);
+   mvprintw(16, 5, "%s", &row[LENGTH - LENGTH*channel[3]/8191]);
+   attroff(COLOR_PAIR(1) | A_INVIS);
+   
+   mvprintw(4, 63, ":%4d", channel[0]);
+   mvprintw(8, 63, ":%4d", channel[1]);
+   mvprintw(12, 63, ":%4d", channel[2]);
+   mvprintw(16, 63, ":%4d", channel[3]);
+   
+   mvprintw(18, 2, "+/-: Switch to increase or decrease mode");
+   mvprintw(19, 2, "1-4:Change value of channel");
+   mvprintw(20, 2, "a:Change all channels");
+   mvprintw(21, 2, "q: Quit");
+   
+   refresh();
+}
+      
+
+int main(int argc, char *argv[])
+{
+   int ch = 0;
+   int i;
+   for (i = 0; i<LENGTH+1; i++)
+	 row[i] = '#';
+   row[0] = 'a';
+   row[LENGTH] = '\0';
+   
+   if (I2Cinit() != TRUE)
+   {
+       printf("Initializing I2C interface failed\n");
+       exit (1);
+   }
+   initscr();
+   if(has_colors() == FALSE)
+	{	endwin();
+		printf("Your terminal does not support color\n");
+		exit(1);
+	}
+   start_color();			/* Start color 			*/
+   init_pair(1, COLOR_BLUE, COLOR_BLUE);
+   cbreak();
+   noecho();
+    
+   setAllChannels();
+   printScreen();
+   int inc = 1;
+   while(ch != 'q')
+   {
+	 ch = getch();
+	 switch(ch)
+	 {
+         case '+':inc = 32; break;
+         case '-':inc = -32; break;
+		 case '1': channel[0]+=inc; 
+                   setSingleChannel(0); break;
+		 case '2': channel[1]+=inc;
+                   setSingleChannel(1); break;
+		 case '3': channel[2]+=inc;
+                   setSingleChannel(2); break;
+		 case '4': channel[3]+=inc;
+                   setSingleChannel(3); break;
+		 case 'a': channel[0]+=inc; 
+				   channel[1]+=inc; 
+				   channel[2]+=inc; 
+				   channel[3]+=inc; 
+                   setAllChannels();    break;
+     }
+     printScreen();
+   }
+   endwin();
+   
+   return 0;
+}
+      
+      
