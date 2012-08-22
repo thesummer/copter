466569af8520bea8223e4b165268034a335e4fc8
Jan Sommer <jsom@hrz.tu-chemnitz.de>
2012-08-22 22:46:32 +0200

    Added doxygen comments to Milestone 1.2

--- avr/Milestone_1.2-pwm/src/main.c
+++ avr/Milestone_1.2-pwm/src/main.c
@@ -1,10 +1,12 @@
-/*
-* written by: Jan Sommer
-* 05.07.2012
+/**
+* @file main.c
+* @author Jan Sommer
+* @date 05.07.2012
 *
-* ATtiny2313
-* Dimms a 4 LEDs connected to OC0A/B and OC1A/B (Pins D5 and B2 - B4)
+* @brief Dimms 4 LEDs connected to OC0A/B and OC1A/B (Pins D5 and B2 - B4)
 *
+* Creates a PWM output on these 4 PINs. The duty cycle of the PWM determines the
+* Brightness of the connected LED. Both timer run in 8-Bit mode.
 */
 
 
@@ -19,32 +21,39 @@
 #define duty4	OCR1B
 
 
+/*!
+ \brief main program for dimming 4 LEDs
+
+ Sets up the pins for the LEDs and switches between 8
+ different brightness levels.
+
+ \return int
+*/
 int main(void)
 {
 
   //Setup the I/O for the LEDs
 
   //LED1
-  DDRD  |= 1<<DDD5;		//Set PortD Pin5 (OC0B) as an output
+  DDRD  |= 1<<DDD5;         //Set PortD Pin5 (OC0B) as an output
 
   //LED2
-  DDRB  |= 1<<DDB2;		//Set PortB Pin2 (OC0A) as an output
-  DDRB  |= 1<<DDB3;		//Set PortB Pin3 (OC1A) as an output
-  DDRB  |= 1<<DDB4;		//Set PortB Pin4 (O1BA) as an output
+  DDRB  |= 1<<DDB2;         //Set PortB Pin2 (OC0A) as an output
+  DDRB  |= 1<<DDB3;         //Set PortB Pin3 (OC1A) as an output
+  DDRB  |= 1<<DDB4;         //Set PortB Pin4 (O1BA) as an output
       
   //Setup the clocks
-  cli();				//Disable global interrupts
+  cli();                    //Disable global interrupts
   
   //Setup Timer0
-//   TCCR0B |= 1<<CS01  | 1<<CS00;	//Divide by 64
-  TCCR0A |= (1<<WGM00);			//Set phase correct PWM
+
+  TCCR0A |= (1<<WGM00);                 //Set phase correct PWM
   TCCR0A |= (1<<COM0A1) | (1<<COM0B1);	//Set both output pins to non-inverting PWM
-  TCCR0B |=  1<<CS01;			//Divide by 8
+  TCCR0B |=  1<<CS01;               	//Divide by 8
   
   //Setup Timer1
-//   TCCR1B |= 1<<CS11 | 1<<CS10;	//Divide by 64
-  TCCR1B |= 1<<CS11;			//Divide by 8
-  TCCR1A |= 1<<WGM10;			//Set phase correct PWM 8-Bit
+  TCCR1B |= 1<<CS11;                //Divide by 8
+  TCCR1A |= 1<<WGM10;               //Set phase correct PWM 8-Bit
   TCCR1A |= 1<<COM1A1 | 1<<COM1B1;	//Activate both output pins to non-inverting pwm
   
   //Initialize duty cycles at the beginning
