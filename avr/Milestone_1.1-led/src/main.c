bb8283528791e1628e12dbfa5361e4174fa22765
Jan Sommer <jsom@hrz.tu-chemnitz.de>
2012-08-22 21:55:05 +0200

    Added Doyxgen comments to Milestone 1.1

--- avr/Milestone_1.1-led/src/main.c
+++ avr/Milestone_1.1-led/src/main.c
@@ -1,7 +1,9 @@
-/*
-* Hackaday.com AVR Tutorial firmware
-* written by: Mike Szczys (@szczys)
-* 10/24/2010
+/**
+*
+* @file main.c
+* @brief Hackaday.com AVR Tutorial firmware
+* @author Mike Szczys (@szczys)
+* @date 10/24/2010
 *
 * ATtiny2313
 * Blinks one LED conneced to PD0
@@ -12,33 +14,47 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
+/**
+ * @brief main program to blink an LED with Attiny2313
+ *
+ * Based on the tutorial of Mike Szczys the program was edited for the
+ * Attiny2313
+ * @return int
+ */
 int main(void)
 {
 
-  //Initialize OC1A pin (Pin PB3) for LED
-  
-  DDRB  |= 1<<DDB3;		//Set PortB Pin3 as an output (OC1A)
-  PORTB |= 1<<PORTB3;		//Set PortB Pin3 high to turn on LED
+  // Initialize OC1A pin (Pin PB3) for LED
+  DDRB  |= 1<<DDB3;             // Set PortB Pin3 as an output (OC1A)
+  PORTB |= 1<<PORTB3;           // Set PortB Pin3 high to turn on LED
       
-  //Setup the clock
-  cli();			//Disable global interrupts
-  TCCR1B |= 1<<CS11 | 1<<CS10;	//Divide by 64
-  OCR1A = 15624;		//Count 15624 cycles for 1 second interrupt
-  TCCR1B |= 1<<WGM12;		//Put Timer/Counter1 in CTC mode
-  TCCR1A |= 1<<COM1A0;		//Toggle OC1A pin on compare match
-//   TIMSK |= 1<<OCIE1A;		//enable timer compare interrupt
-  sei();			//Enable global interrupts
+  // Setup the clock
+  cli();                        // Disable global interrupts
+  TCCR1B |= 1<<CS11 | 1<<CS10;	// Divide by 64
+  OCR1A = 15624;                // Count 15624 cycles for 1 second interrupt
+  TCCR1B |= 1<<WGM12;           // Put Timer/Counter1 in CTC mode
+  TCCR1A |= 1<<COM1A0;          // Toggle OC1A pin on compare match
 
-  //Setup the I/O for the LED
+  /*
+   * Originally the timer interrupt was handled by a ISR which toggled the
+   * pin. Now the pin is automatically toggled on compare match
+   */
+  //   TIMSK |= 1<<OCIE1A;		//enable timer compare interrupt
 
-//   DDRD |= (1<<0);		//Set PortD Pin0 as an output
-//   PORTD |= (1<<0);		//Set PortD Pin0 high to turn on LED
+  sei();                        // Enable global interrupts
 
+// Not needed with auto toogle mode
+/*   Setup the I/O for the LED
+ *    DDRD |= (1<<0);		//Set PortD Pin0 as an output
+ *    PORTD |= (1<<0);		//Set PortD Pin0 high to turn on LED
+ */
   
-  while(1) { }			//Loop forever, interrupts do the rest
+  while(1) { }              // Loop forever, interrupts do the rest
 }
 
-// ISR(TIMER1_COMPA_vect)		//Interrupt Service Routine
-// {
-//   PORTD ^= (1<<0);		//Use xor to toggle the LED
-// }
+// Not needed with auto toogle mode
+/* ISR(TIMER1_COMPA_vect)		//Interrupt Service Routine
+ * {
+ *   PORTD ^= (1<<0);		//Use xor to toggle the LED
+ * }
+ */
